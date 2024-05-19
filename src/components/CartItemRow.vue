2e1c27ab66c4410ceacc11b59423efe98d61d90a
Killer66562 <jimmy688382@gmail.com>
2024-05-19 23:57:14 +0800

    Handled

--- src/components/CartItemRow.vue
+++ src/components/CartItemRow.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
 import { CartItemSchema } from '../models';
 import { getStaticFile } from '../funcs';
+import LoginCheck from './LoginCheck.vue';
+
+const emits = defineEmits<{
+    checkedChanged: []
+}>();
 
 defineProps<{
     cartItem: CartItemSchema
@@ -8,30 +13,39 @@ defineProps<{
 </script>
 
 <template>
+    <LoginCheck>
         <div class="row bg-light pt-1 pb-1">
-            <div class="col-9 col-sm-10 col-md-10">
-                <RouterLink :to="`items/${cartItem.id}`" class="text-decoration-none">
-                    <div class="row">
-                        <div class="col-4 col-sm-3 col-md-2 col-lg-1">
-                            <input type="checkbox"  />
-                            <img class="img-fluid" :src="getStaticFile(cartItem.item.icon)" :alt="`LikedItem #${cartItem.id}`" v-if="cartItem.item.icon" style="aspect-ratio: 1 / 1;">
-                            <img class="img-fluid" src="/src/assets/item.jpg" :alt="`LikedItem #${cartItem.id}`" v-else style="aspect-ratio: 1 / 1;">
-                        </div>
-                        <div class="col-5 col-sm-7 col-md-8 col-lg-9 pt-4">
-                            <div class="row">
-                                <div class="col-7">
-                                    <h5 class="text-start text-dark text-truncate">{{ cartItem.item.name }}</h5>
-                                </div>
-                                <div class="col-5 text-end fw-bold text-danger text-truncate">{{ cartItem.count }}　　　　${{ cartItem.item.price }}</div>
-                            </div>
-                        </div>
+            <div class="col-9 col-sm-10 col-md-11">
+                <div class="row">
+                    <div class="col-auto pt-3">
+                        <input type="checkbox" @change="emits('checkedChanged')" />
                     </div>
-                </RouterLink>
+                    <div class="col-11">
+                        <RouterLink :to="`items/${cartItem.id}`" class="text-decoration-none">
+                            <div class="row">
+                                <div class="col-4 col-sm-3 col-md-2 col-lg-1">
+                                    <img class="img-fluid" :src="getStaticFile(cartItem.item.icon)" :alt="`LikedItem #${cartItem.id}`" v-if="cartItem.item.icon" style="aspect-ratio: 1 / 1;">
+                                    <img class="img-fluid" src="/src/assets/item.jpg" :alt="`LikedItem #${cartItem.id}`" v-else style="aspect-ratio: 1 / 1;">
+                                </div>
+                                <div class="col-8 col-sm-9 col-md-10 col-lg-11 pt-4">
+                                    <div class="row">
+                                        <div class="col-7">
+                                            <h5 class="text-start text-dark text-truncate">{{ cartItem.item.name }}</h5>
+                                        </div>
+                                        <div class="col-3 text-decoration-none">${{ cartItem.item.price }}</div>
+                                        <div class="col-2 text-end fw-bold text-danger text-truncate">{{ cartItem.count }}</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </RouterLink>
+                    </div>
+                </div>
             </div>
-            <div class="col-3 col-sm-2 col-md-2 pt-4">
+            <div class="col-3 col-sm-2 col-md-1 pt-4">
                 <div class="d-flex flex-row justify-content-end">
                     <button class="btn btn-danger btn-sm" type="button">刪除</button>
                 </div>
             </div>
         </div>
+    </LoginCheck>
 </template>
--- src/views/UserCartItemsPage.vue
+++ src/views/UserCartItemsPage.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import { useUserCartItemsStore } from '../stores/userCartItemsStore';
 import { useInfiniteScroll, useAsyncState } from '@vueuse/core';
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import CartItemRow from '../components/CartItemRow.vue';
 import TriState from '../components/TriState.vue';
 import LoginCheck from '../components/LoginCheck.vue';
 import { router } from '../routes';
+import { CartItemSchema } from '../models';
 
 const loader = ref();
 const userCartItemsStore = useUserCartItemsStore();
@@ -17,17 +18,36 @@ useInfiniteScroll(loader, () => fetchState.execute(), { canLoadMore: () => { ret
 const onNotLogin = async () => {
     await router.replace("/login");
 }
+
+const selectedItems = ref<CartItemSchema[]>([]);
+
+const selectedItemsOnChange = (cItem: CartItemSchema) => {
+    const idx = selectedItems.value.findIndex((cartItem) => cartItem.id == cItem.id);
+    if (idx > -1)
+        selectedItems.value.splice(idx);
+    else
+        selectedItems.value.push(cItem);
+}
+
+const totalPrice = computed<number>(() => {
+    let total = 0;
+    selectedItems.value.forEach((cartItem) => {
+        total += cartItem.item.price;
+    });
+    return total;
+});
 </script>
 
 <template>
     <LoginCheck @not-login="onNotLogin">
         <div class="container" ref="loader">
-            <CartItemRow v-for="cartItem in userCartItemsStore.cartItemsData" :key="cartItem.item_id" :cart-item="cartItem" />
+            <CartItemRow v-for="cartItem in userCartItemsStore.cartItemsData" :key="cartItem.item_id" :cart-item="cartItem" @checked-changed="selectedItemsOnChange(cartItem)" />
             <TriState :loading="fetchState.isLoading.value" :ready="fetchState.isReady.value" :error="fetchState.error.value">
                 <template #loading>
                     <h3 class="text-center">讀取中。。。</h3>
                 </template>
             </TriState>
         </div>
+        <h5 class="mt-3">總價：${{ totalPrice }}</h5>
     </LoginCheck>
 </template>
